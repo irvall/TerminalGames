2552dcc52fb235c73a4d327ae034ad9148324db2
irvall <johanirvall@gmail.com>
2018-07-09 18:41:17 +0200

    added players

--- TTT.c
+++ TTT.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 const char empty = ' ';
 int total;
 int m;
 int n;
 int goal;
-int gameover;
 int gamemode;
 int nmoves;
 char xo;
@@ -15,16 +15,29 @@ char *board;
 
 void print_board()
 {
+    printf("\n");
     for (int i = 0; i < total; i++)
     {
         printf("|%c", *(board + i));
-        if ((i + 1) % m == 0)
+        if ((i + 1) % n == 0)
             printf("|\n");
     }
     printf("\n");
 }
 
-void search_winner()
+void print_debug()
+{
+    for (int i = 0; i < total; i++)
+    {
+        if (*(board + i) == empty)
+            printf("- ");
+        else
+            printf("(%i %c) ", i, *(board + i));
+    }
+    printf("\n");
+}
+
+int search_winner()
 {
     int c = 0;
     //upper left to lower right diagonal
@@ -34,10 +47,7 @@ void search_winner()
         if (*(board + i) == *(board + j) && *(board + i) != empty)
         {
             if (++c == goal - 1)
-            {
-                gameover = 1;
-                break;
-            }
+                return 1;
         }
         else
             c = 0;
@@ -45,16 +55,13 @@ void search_winner()
 
     c = 0;
     //lower left to upper right diagonal
-    for (int i = n - 1; i < total-m; i += (n / 2) + 1)
+    for (int i = n - 1; i < total - m; i += (n / 2) + 1)
     {
         int j = i + (n / 2) + 1;
         if (*(board + i) == *(board + j) && *(board + i) != empty)
         {
             if (++c == goal - 1)
-            {
-                gameover = 1;
-                break;
-            }
+                return 1;
         }
         else
             c = 0;
@@ -70,31 +77,26 @@ void search_winner()
             i = j;
             j = i + n;
             if (++c == goal - 1)
-            {
-                gameover = 1;
-                break;
-            }
+                return 1;
         }
         i = temp;
     }
 
-    for (int i = 0; i < total; i += goal)
+    for (int i = 0; i < total - goal; i++)
     {
         c = 0;
         int j = i + 1;
         int temp = i;
-        while (*(board + i) == *(board + j) && *(board + i) != empty)
+        while (*(board + i) == *(board + j) && *(board + i) != empty && i % m == j % m - 1)
         {
             i = j;
             j = i + 1;
             if (++c == goal - 1)
-            {
-                gameover = 1;
-                break;
-            }
+                return 1;
         }
         i = temp;
     }
+    return 0;
 }
 
 void switch_turn()
@@ -124,28 +126,27 @@ int place(int x, int y)
             return 1;
         }
     }
-
 }
 
-//Primitive computer AI
-void make_random_move() 
+void make_random_move()
 {
     int xr = rand() % m;
     int yr = rand() % n;
-    if(place(xr,yr) == 0)
+    if (!place(xr, yr))
         make_random_move();
 }
 
 int main()
 {
-    srand(time(NULL));
     xo = 'O';
-    gameover = 0;
     nmoves = 0;
+    srand(time(NULL));
     printf("Enter dimensions (m,n): ");
     scanf("%d,%d", &m, &n);
     printf("Enter goal: ");
     scanf("%d", &goal);
+    printf("Enter number of players: ");
+    scanf("%d", &gamemode);
     total = m * n;
     board = (char *)malloc(total * sizeof(char));
     for (int i = 0; i < total; i++)
@@ -153,19 +154,37 @@ int main()
 
     int x;
     int y;
-    while (!gameover)
+    while (2 + 2 == 4)
     {
         printf("Enter move (x,y): ");
         if (scanf("%d,%d", &x, &y))
         {
-            if(!place(--x, --y)) {
+            if (!place(--x, --y))
+            {
                 printf("Illegal move!\n");
                 continue;
             }
             print_board();
-            search_winner();
         }
-        if(nmoves == total-1) {
+        if (search_winner())
+        {
+            switch_turn();
+            break;
+        }
+        if (gamemode == 1)
+        {
+            printf("\n...thinking...\n");
+            sleep(1);
+            make_random_move();
+            print_board();
+            if (search_winner())
+            {
+                switch_turn();
+                break;
+            }
+        }
+        if (nmoves == total - 1)
+        {
             printf("Draw!\n");
             return EXIT_SUCCESS;
         }
